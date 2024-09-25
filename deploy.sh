a18dc45abb832c8b54f64fd90a2d797bd64a0627
Chandra Sekhar Midatha <87053960+chandumidatha@users.noreply.github.com>
2024-09-26 02:00:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/elRed-cicd-app-deployment
-# Stop existing Flask app
-sudo pkill -f gunicorn
-
-# Start the app with Gunicorn
-nohup gunicorn --bind 0.0.0.0:5000 app:app > /dev/null 2>&1 &
+python3 app.py
 
